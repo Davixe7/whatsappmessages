76c794a5b9139862e00f5042f1507ba62f753c6f
David <davidguilarte7@gmail.com>
2023-07-26 00:58:09 -0400

    refactored to split into more files

---
+++ api.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router  = express.Router();
+const axios   = require('axios')
+
+const { MessageMedia } = require('whatsapp-web.js')
+const ClientManager = require('./service')
+const clientManager = new ClientManager()
+
+router.get('/create_instance', async (req, res) => {
+  if (clientManager.availableClient.id && !clientManager.availableClient.authenticated) {
+    res.send({ instance_id: clientManager.availableClient.id })
+    return
+  }
+
+  clientManager.createClient();
+
+  res.send({ instance_id: clientManager.availableClient.id })
+})
+
+router.get('/set_webhook', async (req, res) => {
+  if (clientManager.availableClient.id && !clientManager.availableClient.authenticated) {
+    if (!req.query.instance_id) { res.status(422).send({ message: 'instance_id is required' }); return }
+    if (!req.query.webhook_url) { res.status(422).send({ message: 'Webhook url is required' }); return }
+    if (req.query.instance_id != clientManager.availableClient.id) { res.status(422).send({ message: 'Instance ID invalidated' }); return }
+
+    clientManager.availableClient.webhook_url = req.query.webhook_url
+
+    clientManager.availableClient.client.once('ready', () => {
+      console.log(clientManager.availableClient.client.info)
+      axios.get(req.query.webhook_url, {
+        params: { event: 'ready', instance_id: clientManager.availableClient.id, phone: clientManager.availableClient.client.info.wid.user }
+      })
+        .then(res => console.log('Webhook set successfully'))
+        .catch(err => { console.log(err); console.log(err.response) })
+    })
+
+    res.send({ 'message': 'Webhook set successfully', 'status': 'success' })
+    return
+  }
+  res.send('Instance ID invalidated')
+})
+
+router.get('/get_qrcode', (req, res) => {
+  if (!req.query.instance_id) {
+    res.status(422).send('instance_id field is required')
+    return;
+  }
+
+  if (clientManager.availableClient.authenticated || clientManager.availableClient.id != req.query.instance_id) {
+    res.send('instance ID invalidated')
+    return;
+  }
+
+  res.send({ base64: clientManager.availableClient.qr, status: 'success' })
+})
+
+router.get('/find', async (req, res) => {
+  let client = await clientManager.findClient(req.query.instance_id);
+
+  if (!client) {
+    res.send('Session does not exist.')
+    return
+  }
+
+  if (client.info) {
+    res.send({status: 'success', message: 'Client found in memory', info: client.info, state: client.status})
+    return
+  }
+
+  res.send({status: 'success', message: 'Client found in DB', info: 'Not available until ready'})
+  return
+})
+
+router.get('/send', async (req, res) => {
+  if (!req.query.instance_id) {
+    res.status(422).send({ message: 'instance_id field is required' });
+    return
+  }
+
+  if (!req.query.number) {
+    res.status(422).send({ message: 'number field is required' });
+    return
+  }
+
+  if (!req.query.message) {
+    res.status(422).send({ message: 'message field is required' });
+    return
+  }
+
+  let client  = await clientManager.findClient(req.query.instance_id)
+  if (!client) { res.status(404).send({ status: 'error', message: 'Instance id not found or invalidated' }); return; }
+
+  let media   = req.query.media_url ? await MessageMedia.fromUrl(req.query.media_url) : null
+  let options = media ? { media, caption: req.query.message } : {}
+
+  if (client.info) {
+    console.log('Sending message ready')
+    client.sendMessage(`${req.query.number}@c.us`, req.query.message, options)
+  }
+  else {
+    console.log('Sending message deferred')
+    client.once('ready', () => client.sendMessage(`${req.query.number}@c.us`, req.query.message, options))
+  }
+
+  res.send({ message: 'Message sent successfully' })
+  return
+
+})
+
+router.get('/logout', async (req, res) => {
+  let message = await clientManager.logout(req.query.instance_id)
+  res.send({ status: 'success', message })
+})
+
+module.exports = router;
---
+++ service.js
@@ -0,0 +1,94 @@
+const mongoose               = require('mongoose')
+const { MongoStore }         = require('wwebjs-mongo')
+const store                  = new MongoStore(mongoose)
+const { Client, RemoteAuth } = require('whatsapp-web.js')
+const qrcode                 = require('qrcode')
+const { v4: uuidv4 }         = require('uuid')
+
+class ClientManager {
+  constructor() {
+    this.clients = {};
+    this.availableClient = {}
+    mongoose.connect('mongodb://localhost/wweb')
+  }
+
+  createClient() {
+    let clientId     = uuidv4().split('-').pop()
+    let authStrategy = new RemoteAuth({store, clientId, backupSyncIntervalMs: 300000})
+    let client = new Client({authStrategy});
+
+    this.availableClient.authenticated = false
+    this.availableClient.client        = client
+    this.availableClient.id            = clientId
+
+    console.log('Client created ' + client.id);
+
+    this.setClientDefaultListeners(client);
+
+    client.initialize();
+    console.log('Initializing client');
+    return client;
+  }
+
+  setClientDefaultListeners(client) {
+    console.log('Setting client default listeners');
+
+    client.on('qr', (qrcontent) => {
+      qrcode.toDataURL(qrcontent, (err, url) => {
+        this.availableClient.qr = url;
+      });
+    });
+
+    client.on('ready', () => {
+      console.log('Client ready!');
+      if (client.options.authStrategy.clientId == this.availableClient.id) {
+        this.availableClient.authenticated = true;
+      }
+      this.clients[client.options.authStrategy.clientId] = client;
+      console.log('Client info');
+      console.log(client.info);
+    });
+  }
+
+  restoreClient(clientId) {
+    let authStrategy = new RemoteAuth({ store, clientId, backupSyncIntervalMs: 300000 })
+    let client = new Client({ authStrategy })
+    client.initialize()
+    this.clients[clientId] = client;
+    return client;
+  }
+
+  async findClient(instance_id) {
+    let exists = await store.sessionExists({ session: `RemoteAuth-${instance_id}` });
+    if (!exists && !this.clients.hasOwnProperty(instance_id)) return null;
+
+    if (this.clients.hasOwnProperty(instance_id)) {
+      console.log('Client found in memory');
+      return this.clients[instance_id];
+    }
+
+    console.log('Client found in DB');
+    return this.restoreClient(instance_id);
+  }
+
+  async deleteClient(client, instance_id){
+    await client.logout()
+    await store.delete({ session: instance_id });
+    delete this.clients[instance_id];
+  }
+
+  async logout(instance_id){
+    let client = await this.findClient(instance_id)
+    if( !client ){ return 'session already closed.'; }
+
+    if( client.info ){
+      this.deleteClient(client, instance_id)
+      return 'session closed.'
+    }
+
+    client.once('ready', async () => this.deleteClient(client, instance_id))
+    return 'session will close soon.'
+  }
+}
+
+module.exports = ClientManager;
